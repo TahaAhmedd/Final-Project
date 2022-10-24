8a6ef5b5afea75326911c4a01b4d1ae0f99d3e8a
mostafahpater <[mostafahpater@gamil.com>
2022-10-25 01:01:34 +0200

    edit in add jop

--- src/component/AddJops/Addjops.js
+++ src/component/AddJops/Addjops.js
@@ -2,137 +2,147 @@ import React, { useState } from "react";
 import "./Addjops.css";
 import { Formik, useFormik } from "formik";
 import * as yup from "yup";
+import axios from "axios";
+import addpost from "../../images/landing/loginbackground.jpg";
+import { upload } from "@testing-library/user-event/dist/upload";
 function Addjops() {
-    const [values, setValues] = useState({});
+  const [values, setValues] = useState({});
+  const [file, setFile] = useState([]);
 
-    const formik = useFormik({
-        initialValues: {
-            address: "",
-            city: "",
-            title: "",
-            category: "",
-            description: "",
-            cost: "",
-            days: "",
-            upload: "",
-        },
-        validationSchema: yup.object().shape({
-            address: yup.string().required("الرجاء اختيار العنوان"),
-            city: yup.string().required("الرجاء املاء الحقل"),
-            business: yup.string().required("الرجاء اختيار نوع الحرفة"),
-            description: yup
-                .string()
-                .min(15, "Must be 15 characters or more")
-                .required("الرجاء املاء الحقل"),
-            cost: yup.number("الرجاء ادخال رقم").required("الرجاء وضع التكلفة "),
-            days: yup.string().required("الرجاء وضع مدة التسليم"),
-            upload: yup.mixed().required("الرجاء اختيار صورة "),
-        }),
-    });
+  const formik = useFormik({
+    initialValues: {
+      address: "",
+      city: "",
+      title: "",
+      category: "",
+      description: "",
+      cost: "",
+      days: "",
+      jobImage: "",
+    },
+    validationSchema: yup.object().shape({
+      address: yup.string().required("الرجاء اختيار العنوان"),
+      city: yup.string().required("الرجاء املاء الحقل"),
+      title: yup.string().required("الرجاء املاء الحقل"),
+      category: yup.string().required("الرجاء اختيار نوع الحرفة"),
+      description: yup
+        .string()
+        .min(15, "الرجاء كتابة اكثر من 15 كلمة")
+        .required("الرجاء املاء الحقل"),
+      cost: yup.number("الرجاء ادخال رقم").required("الرجاء وضع التكلفة "),
+      days: yup.string().required("الرجاء وضع مدة التسليم"),
+      jobImage: yup.mixed().required("الرجاء اختيار صورة "),
+    }),
+    onSubmit: (values) => {
+      axios.post("http://localhost:7000/jobs/postjob", values);
+    },
+  });
+  //   var imagearr = [];
+  //   const uploadimage = (e) => {
+  //     console.log("zzzzz");
+  //     if (e.target.files && e.target.files.length > 0) {
+  //       setFile(URL.createObjectURL( e.target.files[0]));
+  //       console.log(e.target.files[0].name);
 
-    console.log(formik.values);
-    // console.log(formik.handleSubmit);
-    return (
-        <main>
-            <div className="container">
-                <h1>أضف حرفتك</h1>
-                <form onSubmit={formik.handleSubmit} enctype="multipart/form-data">
-                    <div className="parent-addr">
-                        <div className="main-addr">
-                            <label htmlFor="city" name="اختر مدينتك">
-                                اختر المدينة
-                            </label>
-                            <select
-                                id="city"
-                                name="city"
-                                {...formik.getFieldProps('city')}
-                            >
-                                <option>اختر مدينتك</option>
-                                <option value="أسوان">أسوان</option>
-                                <option value="أسوان الجديدة">أسوان الجديدة</option>
-                                <option value="دراو">دراو</option>
-                                <option value="كوم امبو">كوم امبو</option>
-                                <option value="نصر النوبة">نصر النوبة</option>
-                                <option value="كلابشة">كلابشة</option>
-                                <option value="أدفو">أدفو</option>
-                            </select>
-                            {formik.touched.city && formik.errors.city ? (
-                                <div style={{ color: "red" }}>{formik.errors.city}</div>
-                            ) : null}
-                        </div>
+  //     }
+  //   };
 
+  //   console.log(formik.values);
+  // console.log(formik.handleSubmit);
+  return (
+    <main>
+      <div className="container">
+        <form onSubmit={formik.handleSubmit} encType="multipart/form-data">
+          <h1>أضف مشكلتك</h1>
+          <div className="parent-addr">
+            <div className="main-addr">
+              <label htmlFor="city" name="اختر مدينتك">
+                اختر المدينة
+              </label>
+              <select id="city" name="city" {...formik.getFieldProps("city")}>
+                  <optgroup label="مدينة أسوان">
+                    <option value="0" selected>أختر المركز</option>
+                    <option value="أسوان">أسوان</option>
+                    <option value="أسوان الجديدة">أسوان الجديدة</option>
+                    <option value="دراو">دراو</option>
+                    <option value="كوم امبو">كوم امبو</option>
+                    <option value="نصر النوبة">نصر النوبة</option>
+                    <option value="كلابشة">كلابشة</option>
+                    <option value="أدفو">أدفو</option>
+                  </optgroup>
+              </select>
+              {formik.touched.city && formik.errors.city ? (
+                <div style={{ color: "red" }}>{formik.errors.city}</div>
+              ) : null}
+            </div>
 
-                        <div className="detail-addr">
-                            <label htmlFor="title">عنوان الوظيفة</label>
-                            <input
-                                type="text"
+            <div className="detail-addr" >
+              <label htmlFor="title">عنوان الوظيفة</label>
+              <input
+                type="text"
+                {...formik.getFieldProps("title")}
+                id="title"
+                name="title"
+                placeholder="اكتب عنوانك بالتفصيل"
+              />
+              {formik.touched.title && formik.errors.title ? (
+                <div style={{ color: "red" }}>{formik.errors.title}</div>
+              ) : null}
+            </div>
 
-                                {...formik.getFieldProps('title')}
-                                id="title"
-                                name="title"
-                                placeholder="اكتب عنوانك بالتفصيل"
-                            />
-                            {formik.touched.title && formik.errors.title ? (
-                                <div style={{ color: "red" }}>{formik.errors.title}</div>
-                            ) : null}
-                        </div>
+            <div className="detail-addr">
+              <label htmlFor="address">عنوانك</label>
+              <input
+                type="text"
+                {...formik.getFieldProps("address")}
+                id="address"
+                name="address"
+                placeholder="اكتب عنوانك بالتفصيل"
+              />
+              {formik.touched.address && formik.errors.address ? (
+                <div style={{ color: "red" }}>{formik.errors.address}</div>
+              ) : null}
+            </div>
+          </div>
 
+          <div className="business">
+            <label htmlFor="business">اختر الحرفة</label>
+            <select
+              id="business"
+              name="category"
+              {...formik.getFieldProps("category")}
+            >
+              <option value="0">أختر الحرفة</option>
+              <option value="نجار">نجار</option>
+              <option value="سباك">سباك</option>
+              <option value="مبيض محارة">مبيض محارة</option>
+              <option value="كهربائي">كهربائي</option>
+              <option value="فني تكييف">فني تكييف</option>
+              <option value="نقاش">نقاش</option>
+              <option value="بناء">بناء</option>
+              <option value="الوميتال">الوميتال</option>
+            </select>
+            {formik.touched.category && formik.errors.category ? (
+              <div style={{ color: "red" }}>{formik.errors.category}</div>
+            ) : null}
+          </div>
 
-                        <div className="detail-addr">
-                            <label htmlFor="address">عنوانك</label>
-                            <input
-                                type="text"
+          <div className="descript-jop">
+            <label htmlFor="descript">تفاصيل مشكلتك</label>
+            <textarea
+              name="description"
+              {...formik.getFieldProps("description")}
+              id="descript"
+              cols="30"
+              rows="10"
+              placeholder="اكتب تفاصيل مشكلتك ...."
+            ></textarea>
+            {formik.touched.description && formik.errors.description ? (
+              <div style={{ color: "red" }}>{formik.errors.description}</div>
+            ) : null}
+          </div>
 
-                                {...formik.getFieldProps('address')}
-                                id="address"
-                                name="address"
-                                placeholder="اكتب عنوانك بالتفصيل"
-                            />
-                            {formik.touched.address && formik.errors.address ? (
-                                <div style={{ color: "red" }}>{formik.errors.address}</div>
-                            ) : null}
-                        </div>
-                    </div>
-
-
-                    <div className="business">
-                        <label htmlFor="business">اختر الحرفة</label>
-                        <select
-                            id="business"
-                            name="category"
-                            {...formik.getFieldProps('business')}
-                        >
-                            <option>اختر الحرفة</option>
-                            <option value="سباكة">سباكة</option>
-                            <option value="دهانات">دهانات</option>
-                            <option value="نجارة">نجارة</option>
-                            <option value="كهرباء">كهرباء</option>
-                            <option value="بلاط ورخام">بلاط ورخام</option>
-                            <option value="جبس ومحارة وكرانيش">جبس ومحارة وكرانيش</option>
-                            <option value="زجاج">زجاج</option>
-                            <option value="تبريد وتكييف">تبريد وتكييف</option>
-                        </select>
-                        {formik.touched.category && formik.errors.category ? (
-                            <div style={{ color: "red" }}>{formik.errors.category}</div>
-                        ) : null}
-                    </div>
-
-
-                    <div className="descript-jop">
-                        <label htmlFor="descript">تفاصيل حرفتك</label>
-                        <textarea
-                            name="description"
-                            {...formik.getFieldProps('description')}
-                            id="descript"
-                            cols="30"
-                            rows="10"
-                        ></textarea>
-                        {formik.touched.description && formik.errors.description ? (
-                            <div style={{ color: "red" }}>{formik.errors.description}</div>
-                        ) : null}
-                    </div>
-
-                    {/* 
+          {/* 
                     <div className="price">
                         <div className="cost-day">
                             <label htmlFor="cost">السعر</label>
@@ -164,30 +174,37 @@ function Addjops() {
                         </div>
                     </div> */}
 
+          <div className="add-pic">
+            <label htmlFor="upload-files">
+              <i className="fa fa-download fs-5 " aria-hidden="true">
+                اضف صورة
+              </i>
+            </label>
 
-                    <div className="add-pic">
-                        <label htmlFor="upload-files">اضف صورة او فيديو</label>
-
-                        <input
-                            type="file"
-                            {...formik.getFieldProps('upload')}
-                            // style={{ display: "none" }}
-                            name="jobImage"
-                            defaultValue="upload"
-                            id="upload-files"
-                            accept="image/png, image/jpg, image/gif, image/jpeg"
-                            multiple
-                        />
-                        {formik.touched.upload && formik.errors.upload ? (
-                            <div style={{ color: "red" }}>{formik.errors.upload}</div>
-                        ) : null}
-                    </div>
-
-                    <input type="submit" id="submit" value="اضف حرفتك" />
-                </form>
+            <input
+              type="file"
+              {...formik.getFieldProps("jobImage")}
+              //   onChange={uploadimage}
+              style={{ display: "none" }}
+              name="jobImage"
+              // defaultValue="upload"
+              id="upload-files"
+              accept="image/*"
+              multiple
+            />
+            <div>
+             
             </div>
-        </main>
-    );
+            {formik.touched.upload && formik.errors.upload ? (
+              <div style={{ color: "red" }}>{formik.errors.upload}</div>
+            ) : null}
+          </div>
+
+          <input type="submit" id="submit" value="اضف حرفتك" />
+        </form>
+      </div>
+    </main>
+  );
 }
 
 export default Addjops;
