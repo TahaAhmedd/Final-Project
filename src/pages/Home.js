a4a586781620594a90a0c33f5ec0d7fbc224f29f
TahaAhmedd <teto67523@gmail.com>
2022-11-02 21:51:34 +0200

    add changes in search

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ function Home() {
   const [alldata, setAll] = useState([])
   const [loader, setLoader] = useState(true)
   const [flag, setFlag] = useState(false);
-
+  console.log(alldata)
   useEffect(() => {
 
     axios.get("http://localhost:7000/jobs/all").then(
@@ -57,7 +57,8 @@ function Home() {
 
   function search(type) {
     let arr = alldata.filter((item) => {
-      return item.description.includes(type)
+      return item.description.includes(type) || item.title.includes(type) || item.category.includes(type) ||
+      item.city.includes(type)
     }
     )
     setData([...arr])
